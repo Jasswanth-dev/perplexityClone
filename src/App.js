34d07877e8a22e25f93036f9939afb9439f46329
Jasswanth-dev <jasswanth3@gmail.com>
2024-10-10 19:42:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Sidebar from './Components/Sidebar/index';
+
+import Discover from './Components/discover/index';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-container">
+      <Sidebar/>
+      <Discover/>
+    </div>
+  );
+}
+
+export default App;
